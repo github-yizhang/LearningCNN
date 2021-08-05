907d13ab0c064b44e956597352c4e7d70d1d7d2a
zhangyi <2512984629@qq.com>
2021-08-05 20:40:13 +0800

    ipynb

--- Alexnet.py
+++ Alexnet.py
@@ -1,4 +1,3 @@
-
 import torch
 from torch import nn
 from d2l import torch as d2l
@@ -32,4 +31,3 @@ batch_size = 128
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
 lr, num_epochs = 0.01, 10
 d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
-
